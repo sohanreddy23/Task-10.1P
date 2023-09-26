227fba153dbdcd73e47557d4365fb14465930fc4
Sohan Reddy <sohanreddy@Sohans-MacBook-Air.local>
2023-09-26 17:56:58 +0530

    maybe

--- src/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Input = (props)=>{
-    return <div>
-       <input name={props.name} type={props.type} placeholder={props.placeholder} onChange = {props.onChange}/>
-    </div>
-
-}
-export default Input
